c4a804b95d6b4ebea11ed2dc85fc9536bba14938
KPolish <com94ok@naver.com>
2018-05-08 12:54:15 +0900

    20180508_Vue#02

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+        <section class="section">
+            <div class="block">
+                about page
+            </div>
+        </section>
+    </template>
--- pages/default.vue
+++ pages/default.vue
@@ -3,6 +3,10 @@
         <div class="block">
             <a href="#" class="button is-primary">Test</a><br>
             <p>클라우드 서비스용 포트폴리오</p>
+            <img src="~static/logo.gif" alt=""><br>
+            <img src="~assets/logo.gif" alt="">
         </div>
+        <br>
+        <nuxt-link to="/about">About</nuxt-link>
     </section>
 </template>
