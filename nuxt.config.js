8c12eb81ebf74727a594f5a50d1b7e32657f982d
KPolish <com94ok@naver.com>
2018-05-08 12:57:04 +0900

    20180508_Vue#02

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel="stylesheet", href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css"  }
-    ]
+    ],
+    script: [{ src: 'https://use.fontawesome.com/releases/v5.0.7/js/all.js', defer: '' }]
+  
   },
   /*
   ** Customize the progress bar color
